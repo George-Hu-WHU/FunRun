0b436106fff19d868f1c0aa0455f74eb4a16c2fd
DuPeng <3432751916@qq.com>
2023-09-27 16:09:58 +0800

    问卷代码修改优化

--- miniprogram/pages/questionaire/questionaire.js
+++ miniprogram/pages/questionaire/questionaire.js
@@ -15,71 +15,60 @@ Page({
     rate: "",
     object: "",
     experience: "",
-    genders: [{
-      val: "男"
-    }, {
-      val: "女"
-    }],
+
+    genders: [
+      {name:"男", val: "男"},
+      {name:"女", val: "女"}
+    ],
+
     ages: [
-      {name:"18岁以下", val: "18岁以下", checked: true},
+      {name:"18岁以下", val: "18岁以下"},
       {name:"18~30岁", val: "18~30岁"},
       {name:"30~45岁", val: "30~45岁"},
       {name:"45岁以上", val: "45岁以上"}
     ],
-    weight: [{
-      val: "40kg以下"
-    }, {
-      val: "40~60kg"
-    }, {
-      val: "60~80kg"
-    }, {
-      val: "80kg以上"
-    }, ],
-    hight: [{
-      val: "160cm以下"
-    }, {
-      val: "160~170cm"
-    }, {
-      val: "170~180cm"
-    }, {
-      val: "180cm以上"
-    }, ],
-    healthy_conditions: [{
-      val: "健康状态良好"
-    }, {
-      val: "健康状态轻微问题"
-    }, {
-      val: "健康状态严重问题"
-    }],
-    runrate: [{
-      val: "1次"
-    }, {
-      val: "2次"
-    }, {
-      val: "3次"
-    }, {
-      val: "4次或更多"
-    }, ],
-    objects: [{
-      val: "减肥"
-    }, {
-      val: "提高耐力"
-    }, {
-      val: "增强肌肉力量"
-    }, {
-      val: "改善心肺健康"
-    }, {
-      val: "其他"
-    }],
-    experiences: [{
-      val: "完全没有经验"
-    }, {
-      val: "有少量经验，但不常跑步"
-    }, {
-      val: "经常跑步，有一定经验"
-    }, {
-      val: "超级跑者，经验丰富"
-    }],
+
+    weight: [
+      {name:"40kg以下", val: "40kg以下"}, 
+      {name:"40~60kg", val: "40~60kg"}, 
+      {name:"60~80kg", val: "60~80kg"}, 
+      {name:"80kg以上", val: "80kg以上"}
+    ],
+
+    hight: [
+      {name:"160cm以下", val: "160cm以下"}, 
+      {name:"160~170cm", val: "160~170cm"}, 
+      {name:"170~180cm", val: "170~180cm"}, 
+      {name:"180cm以上", val: "180cm以上"}
+    ],
+
+    healthy_conditions: [
+      {name:"健康状态良好", val: "健康状态良好"}, 
+      {name:"健康状态轻微问题", val: "健康状态轻微问题"}, 
+      {name:"健康状态严重问题", val: "健康状态严重问题"}
+    ],
+
+    runrate: [
+      {name:"1次", val: "1次"}, 
+      {name:"2次", val: "2次"}, 
+      {name:"3次", val: "3次"}, 
+      {name:"4次或更多", val: "4次或更多"}
+    ],
+
+    objects: [
+      {name:"减肥", val: "减肥"}, 
+      {name:"提高耐力", val: "提高耐力"}, 
+      {name:"增强肌肉力量", val: "增强肌肉力量"}, 
+      {name:"改善心肺健康", val: "改善心肺健康"}, 
+      {name:"其他", val: "其他"}
+    ],
+
+    experiences: [
+      {name:"完全没有经验", val: "完全没有经验"}, 
+      {name:"有少量经验，但不常跑步", val: "有少量经验，但不常跑步"}, 
+      {name:"经常跑步，有一定经验", val: "经常跑步，有一定经验"}, 
+      {name:"超级跑者，经验丰富", val: "超级跑者，经验丰富"}
+    ],
   },
 
   //调用云函数获取openid
@@ -105,21 +94,76 @@ Page({
     })
   },
 
-  //问卷结果上传数据库
-  formsubmit: function (e) {
+  bindage: function (e) {
+    const age = e.detail.value;
     this.setData({
-      gender: e.detail.value.gender,
-      age: e.detail.value.age,
-      kg: e.detail.value.kg,
-      cm: e.detail.value.cm,
-      healthy_condition: e.detail.value.healthy_condition,
-      rate: e.detail.value.rate,
-      object: e.detail.value.object,
-      experience: e.detail.value.experience
-    })
+      age : age,
+    });
+  },
+
+  bindgender: function (e) {
+    const gender = e.detail.value;
+    this.setData({
+      gender : gender,
+    });
+  },
+
+  bindkg: function (e) {
+    const kg = e.detail.value;
+    this.setData({
+      kg : kg,
+    });
+  },
+
+  bindcm: function (e) {
+    const cm = e.detail.value;
+    this.setData({
+      cm : cm,
+    });
+  },
+
+  bindhealthy_condition: function (e) {
+    const healthy_condition = e.detail.value;
+    this.setData({
+      healthy_condition : healthy_condition,
+    });
+  },
+
+  bindrate: function (e) {
+    const rate = e.detail.value;
+    this.setData({
+      rate : rate,
+    });
+  },
+
+  bindobject: function (e) {
+    const object = e.detail.value;
+    this.setData({
+      object : object,
+    });
+  },
+
+  bindexperience: function (e) {
+    const experience = e.detail.value;
+    this.setData({
+      experience : experience,
+    });
+  },
+
+  //问卷结果上传数据库
+  submitForm:function(){
+    const gender = this.data.gender;
+    const age = this.data.age;
+    const kg = this.data.kg;
+    const cm =  this.data.cm;
+    const healthy_condition =  this.data.healthy_condition;
+    const rate =  this.data.rate;
+    const object = this.data.object;
+    const experience = this.data.experience;
+
     const db = wx.cloud.database()
     //数据存在未选择项，不许提交
-    if (e.detail.value.gender.length == 0 || e.detail.value.age.length == 0 || e.detail.value.kg.length == 0 || e.detail.value.cm.length == 0 || e.detail.value.healthy_condition.length == 0 || e.detail.value.rate.length == 0 || e.detail.value.object.length == 0 || e.detail.value.experience.length == 0) {
+    if (gender.length == 0 || age.length == 0 || kg.length == 0 || cm.length == 0 || healthy_condition.length == 0 || rate.length == 0 || object.length == 0 || experience.length == 0) {
       wx.showToast({
         title: '请完成全部选择',
         icon: 'loading',
@@ -140,14 +184,14 @@ Page({
             _openid: openid
           }).update({
             data: {
-              gender: e.detail.value.gender,
-              age: e.detail.value.age,
-              kg: e.detail.value.kg,
-              cm: e.detail.value.cm,
-              healthy_condition: e.detail.value.healthy_condition,
-              rate: e.detail.value.rate,
-              object: e.detail.value.object,
-              experience: e.detail.value.experience
+              gender: gender,
+              age: age,
+              kg: kg,
+              cm: cm,
+              healthy_condition: healthy_condition,
+              rate: rate,
+              object: object,
+              experience: experience
             },
             success: function (res) {
               wx.showToast({
@@ -170,14 +214,14 @@ Page({
         fail(res) {
           db.collection('questionaire_data').add({
             data: {
-              gender: e.detail.value.gender,
-              age: e.detail.value.age,
-              kg: e.detail.value.kg,
-              cm: e.detail.value.cm,
-              healthy_condition: e.detail.value.healthy_condition,
-              rate: e.detail.value.rate,
-              object: e.detail.value.object,
-              experience: e.detail.value.experience
+              gender: gender,
+              age: age,
+              kg: kg,
+              cm: cm,
+              healthy_condition: healthy_condition,
+              rate: rate,
+              object: object,
+              experience: experience
             },
             success(res) {
               wx.showToast({
@@ -200,7 +244,7 @@ Page({
   },
 
   //页面重置
-  formreset: function (e) {
+  formreset:function(e){
     this.setData({
       gender: "",
       age: "",
